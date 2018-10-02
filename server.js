65eaec77720c8a03a40979697ed0251ab7ba4699
Tom King <design.tomking@gmail.com>
2018-10-01 21:10:00 -0600

    Add body-parser to app
    Import colors api funcs

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const cors = require('express-cors');
-const port = (process.env.PORT || 3000);
-const app = express();
+var express = require('express');
+var cors = require('express-cors');
+var port = process.env.PORT || 3000;
+var app = express();
+var bodyParser = require('body-parser');
+var colors = require('./routes/colorsAPI');
 
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/', colors);
+
+app.listen(port);
+
+console.log(`Listening at http://localhost:${port}`);
