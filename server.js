f418cd9a905caa729cb51705d6c680b336a9a20a
Tom King <design.tomking@gmail.com>
2018-10-01 18:25:51 -0600

    Add express-cors to server

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = require('express');
+const cors = require('express-cors');
+const port = (process.env.PORT || 3000);
 const app = express();
+
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
