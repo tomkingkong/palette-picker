e0c1524c39d10a5084ba0727c8cacccd0a512bc5
Tom King <design.tomking@gmail.com>
2018-10-02 18:56:17 -0600

    Add generatepalette func to randomly generate gems

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,9 +3,25 @@ const dropContent = document.querySelector('.dropdown-content');
 const GENERATOR = document.querySelector('.GENERATOR');
 const randomPalette = document.querySelector('.RANDOM__PALETTE');
 const colors = document.querySelectorAll('.COLOR');
+
+GENERATOR.addEventListener('click', function() {
+  generatePalette();
+});
 dropDown.addEventListener('click', toggleDrop);
 dropContent.addEventListener('click', selectProject);
 randomPalette.addEventListener('click', lockColor);
+
+function generatePalette() {
+  colors.forEach((color, i) => {
+    if (color.className.includes('locked')) {
+      return 
+    } else {
+      const gem = new Gem();
+      generateGemClasses(`${gem.shape}`,`${gem.shape+i}`, gem.color);
+      color.innerHTML = `<div class="${gem.shape+i} ${gem.shape}" />`;
+    }
+  })
+}
 
 function lockColor(e) {
   const { className, classList, parentNode } = e.target;
@@ -28,7 +44,7 @@ function selectProject(e) {
 window.onclick = function(event) {
   if (!event.target.matches('.drop_btn')) {
 
-    const dropdowns = document.getElementsByClassName("dropdown-content");
+    const dropdowns = document.getElementsByClassName('dropdown-content');
     for (let i = 0; i < dropdowns.length; i++) {
       let openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
