659b393924786524172e935d70a1b2a6394e1718
Tom King <design.tomking@gmail.com>
2018-10-02 18:58:31 -0600

    Add generateColor func to Gem class

--- public/js/classes.js
+++ public/js/classes.js
@@ -3,5 +3,8 @@ class Gem {
     this.shape = this.generateShape();
     this.color = this.generateColor();
   }
-
+  
+  generateColor() {
+    return '#'+((Math.random()*16777215)<<0).toString(16);
+  }
 }
