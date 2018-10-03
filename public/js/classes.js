2536e3ac426f0aab4a5cd59e5654e99c85fd61ad
Tom King <design.tomking@gmail.com>
2018-10-02 18:59:01 -0600

    Add generateShape func to Gem

--- public/js/classes.js
+++ public/js/classes.js
@@ -3,8 +3,19 @@ class Gem {
     this.shape = this.generateShape();
     this.color = this.generateColor();
   }
-  
+
   generateColor() {
     return '#'+((Math.random()*16777215)<<0).toString(16);
+  }
+
+  generateShape() {
+    const shapes = [ 
+      `DIAMOND`, 
+      `CUT_DIAMOND`, 
+      `TRIANGLE`, 
+      `HEXAGON`, 
+      `OCTAGON` 
+    ]
+    return shapes[(Math.random()*shapes.length-1)+0.5<<0];
   }
 }
