866bd6a0bfc76ab576316784a1790dcc53f5c08c
Tom King <design.tomking@gmail.com>
2018-10-02 19:00:49 -0600

    Add ridiculous long switch statement to handle gem color and shape props

--- public/js/classes.js
+++ public/js/classes.js
@@ -20,6 +20,67 @@ class Gem {
   }
 }
 
+const generateGemClasses = (type, shape, color) => {
+  switch(type) {
+    case 'CUT_DIAMOND':{
+      return (
+        addGemRules(`.${shape}:after`, {
+          'border-color': `${color} transparent transparent transparent`,
+          content: ''
+        }),
+        addGemRules(`.${shape}`, {
+          'border-color': `transparent transparent ${color} transparent`,
+          content: ''
+        }));}
+    case 'DIAMOND':{
+      return (
+        addGemRules(`.${shape}:after`, {
+          'border-top-color': `${color}`,
+          content: ''
+        }),
+        addGemRules(`.${shape}`, {
+          'border-bottom-color': `${color}`,
+          content: ''
+        }));}
+    case 'TRIANGLE':{
+      return (
+        addGemRules(`.${shape}`, {
+          'border-bottom': `100px solid ${color}`,
+          content: ''
+        }));}
+    case 'HEXAGON':{
+      return (
+        addGemRules(`.${shape}`, {
+          'background': `${color}`,
+          content: ''
+        }),
+        addGemRules(`.${shape}:after`, {
+          'border-top': `25px solid ${color}`,
+          content: ''
+        }),
+        addGemRules(`.${shape}:before`, {
+          'border-bottom': `25px solid ${color}`,
+          content: ''
+        }));}
+    case 'OCTAGON':{
+      return (
+        addGemRules(`.${shape}`, {
+          'background': `${color}`,
+          content: ''
+        }),
+        addGemRules(`.${shape}:after`, {
+          'border-top': `29px solid ${color}`,
+          content: ''
+        }),
+        addGemRules(`.${shape}:before`, {
+          'border-bottom': `29px solid ${color}`,
+          content: ''
+        }));}
+    default:
+      return;
+  }
+}
+
 const addGemRules = ((style) => {
   const sheet = document.head.appendChild(style).sheet;
   return (selector, css) => {
