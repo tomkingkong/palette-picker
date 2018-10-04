ab33a97a3b6db38beed1f952ff7b1edc1cc54192
Tom King <design.tomking@gmail.com>
2018-10-03 19:42:21 -0600

    Update syntax to fetch calls

--- public/js/api.js
+++ public/js/api.js
@@ -7,17 +7,20 @@ const fetchCurry = (...paths) => (options, method, body) => {
   }
   return fetch(url, load)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    console.log(data)
+    return data
+  })
   .catch(error => console.log(error));
 }
 
 const getAllProjects = () => fetchCurry('/api/v1/projects')({});
-const getAllColors = () => fetchCurry('/api/v1/colors')({});
-const getProjectPalettes = (id) => fetchCurry(`/api/v1/${id}/palettes`)({});
+// const getAllColors = () => fetchCurry('/api/v1/colors')({});
+const getProjectPalettes = (projectId) => fetchCurry(`/api/v1/${projectId}/palettes`)({});
 
-const addColor = (hex) => fetchCurry(`/api/v1/colors/new`)(false, 'POST', {hex});
-const addProject = (name) => fetchCurry(`/api/v1/projects/new`)(false, 'POST', {name});
-const addPalette = (projectId, palette) => fetchCurry(`/api/v1/${projectId}/palettes/new`)(false, 'POST', {palette});
+const addColor = (shape, hex) => fetchCurry(`/api/v1/colors`)(false, 'POST', {shape, hex});
+const addProject = (name) => fetchCurry(`/api/v1/projects`)(false, 'POST', {name});
+const addPalette = (projectId, palette) => fetchCurry(`/api/v1/${projectId}/palettes`)(false, 'POST', {...palette});
 
 const deletePalette = (projectId, paletteId) => fetchCurry(`/api/v1/${projectId}/palettes/${paletteId}`)(false, 'DELETE');
 const deleteProject = (projectId) => fetchCurry(`/api/v1/${projectId}`)(false, 'DELETE');
