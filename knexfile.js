3dbb4f95ec0e05a6a04295d46c50dd8bd77896ae
Tom King <design.tomking@gmail.com>
2018-10-04 11:37:03 -0600

    Add production environment to knex

--- knexfile.js
+++ knexfile.js
@@ -28,20 +28,13 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
 
 };
