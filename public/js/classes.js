a87c84394339845741122dd34509d6e349c8f507
Tom King <design.tomking@gmail.com>
2018-10-06 14:21:45 -0600

    Remove function to generate gem styles
    Replace hex generator with hsla

--- public/js/classes.js
+++ public/js/classes.js
@@ -5,7 +5,7 @@ class Gem {
   }
 
   generateColor() {
-    return '#'+((Math.random()*16777215)<<0).toString(16);
+    return 'hsla(' + (Math.random() * 360) + ', 85%, 50%, 1)';
   }
 
   generateShape() {
@@ -19,74 +19,3 @@ class Gem {
     return shapes[(Math.random()*shapes.length-1)+0.5<<0];
   }
 }
-
-const generateGemClasses = (type, shape, color) => {
-  switch(type) {
-    case 'CUT_DIAMOND':{
-      return (
-        addGemRules(`.${shape}:after`, {
-          'border-color': `${color} transparent transparent transparent`,
-          content: ''
-        }),
-        addGemRules(`.${shape}`, {
-          'border-color': `transparent transparent ${color} transparent`,
-          content: ''
-        }));}
-    case 'DIAMOND':{
-      return (
-        addGemRules(`.${shape}:after`, {
-          'border-top-color': `${color}`,
-          content: ''
-        }),
-        addGemRules(`.${shape}`, {
-          'border-bottom-color': `${color}`,
-          content: ''
-        }));}
-    case 'TRIANGLE':{
-      return (
-        addGemRules(`.${shape}`, {
-          'border-bottom': `100px solid ${color}`,
-          content: ''
-        }));}
-    case 'HEXAGON':{
-      return (
-        addGemRules(`.${shape}`, {
-          'background': `${color}`,
-          content: ''
-        }),
-        addGemRules(`.${shape}:after`, {
-          'border-top': `25px solid ${color}`,
-          content: ''
-        }),
-        addGemRules(`.${shape}:before`, {
-          'border-bottom': `25px solid ${color}`,
-          content: ''
-        }));}
-    case 'OCTAGON':{
-      return (
-        addGemRules(`.${shape}`, {
-          'background': `${color}`,
-          content: ''
-        }),
-        addGemRules(`.${shape}:after`, {
-          'border-top': `29px solid ${color}`,
-          content: ''
-        }),
-        addGemRules(`.${shape}:before`, {
-          'border-bottom': `29px solid ${color}`,
-          content: ''
-        }));}
-    default:
-      return;
-  }
-}
-
-const addGemRules = ((style) => {
-  const sheet = document.head.appendChild(style).sheet;
-  return (selector, css) => {
-    const propText = Object.keys(css)
-    .map(prop => prop+':'+css[prop])
-    .join(';');
-    sheet.insertRule(`${selector}{ ${propText} }`, sheet.cssRules.length);
-  }
-})(document.createElement("style"));
